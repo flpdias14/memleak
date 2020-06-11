cdd398128e0f45e1bcdcdf6e03d76bdc3b428df5
Felipe Oliveira <flpdias14@gmail.com>
2020-06-11 15:19:50 -0300

    monitor de memória

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 # include <stdio.h>
-# define SIZE 100
+# include <stdlib.h>
+# include <unistd.h>
+
+# define SIZE 10
 float* vector;
 
 
 int main()
 {
+    int count = 0;
+    long int allocaded_size = 0;
     while(1)
     {
-        vector = malloc(sizeof(float)*SIZE)
+        allocaded_size += 1024*SIZE;
+        vector = (float*) malloc(1024*SIZE);
+        // printf("Malloc %d\tsizeof = %d bytes\n", ++count, allocaded_size);
+        //  sleep(1);
+        usleep(10000);
+        
     }
 }
