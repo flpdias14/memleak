5da514a1619463cee026ae88836d1bbb0c7d6526
Felipe Oliveira <flpdias14@gmail.com>
2020-06-11 15:30:00 -0300

    check valgrind

--- run.sh
+++ run.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-rm -f size.log valgrind-out.txt
 
-
-gcc -g -o xmemleak main.c
-
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./xmemleak &
-
-pid_of_mem_leak=`ps -ef | grep [x]memleak | awk '{print $2}'`
-
-
-
-
-function log_memory {
+function log_memory 
+{
     ps --no-headers -o size -p $pid_of_mem_leak >> size.log
 }    
 
-function interrupt()
+function interrupt
 {
     log_memory
     kill -9 $pid_of_mem_leak
@@ -26,8 +16,24 @@ function interrupt()
 
 trap interrupt INT
 
-while [ True ]
-do
-log_memory
+valgrind --version
+t=`echo $?` 
+if [ $t == '0' ];
+then
+    rm -f size.log valgrind-out.txt
+
+
+    gcc -g -o xmemleak main.c
+
+    valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./xmemleak &
+
+    pid_of_mem_leak=`ps -ef | grep [x]memleak | awk '{print $2}'`    
+
+    while [ True ]
+    do
+    log_memory
 
 done 
+else
+    echo "no"
+fi
