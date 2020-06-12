ba5928d41bbeab5c02060176a878dc6cf1a0ca18
Felipe Oliveira <flpdias14@gmail.com>
2020-06-12 17:00:21 -0300

    Rejuvenation

--- run.sh
+++ run.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-
+pid_of_mem_leak=0
+# log memory size in kilobytes
 function log_memory 
 {
     ps --no-headers -o size -p $pid_of_mem_leak >> size.log
 }    
 
+function kill_process
+{
+    kill $pid_of_mem_leak
+}
+
+function restore_aplication
+{
+    valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=valgrind-out.txt ./xmemleak --restore &
+    pid_of_mem_leak=`ps -ef | grep [x]memleak | awk '{print $2}'` 
+}
 function interrupt
 {
     log_memory
-    kill $pid_of_mem_leak
+    kill_process
     echo -e "\n\nEnded."
     log_memory
     exit
@@ -24,6 +35,7 @@ then
 
     gcc -Wall -pedantic -g -o xmemleak main.c
 
+    # start application
     valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --log-file=valgrind-out.txt ./xmemleak &
 
     pid_of_mem_leak=`ps -ef | grep [x]memleak | awk '{print $2}'`    
@@ -31,7 +43,12 @@ then
     while [ True ]
     do
     log_memory
-
+    last=`tail -n 1 size.log`
+    if [ $last -gt 2097152 ]; # 2GB = 2097152 KB
+    then
+        kill_process
+        restore_aplication
+    fi
     done 
 else
     echo "no"
